17ea98a6fa73fdf6fdab12f62fae742ec5d461f9
Jon Lynch <jonnylynchy@gmail.com>
2025-01-07 20:53:09 -0600

    added title to ad component

--- src/components/Advertisement.tsx
+++ src/components/Advertisement.tsx
@@ -7,6 +7,7 @@ interface AdvertisementProps {
 function Advertisement(props: AdvertisementProps) {
   return (
     <div className="advertisement">
+      <h2>Cool Advertisement</h2>
       <p>{props.companyName}</p>
       <p>{props.productDescription}</p>
       <p>{props.price}</p>
